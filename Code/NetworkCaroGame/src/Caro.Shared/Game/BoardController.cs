4377ed10668e6467cc6602a6010704573bdf2234
triet <triettm1833@ut.edu.vn>
2026-04-06 23:09:44 +0700

    update BoardController

--- Code/NetworkCaroGame/src/Caro.Shared/Game/BoardController.cs
+++ Code/NetworkCaroGame/src/Caro.Shared/Game/BoardController.cs
@@ -1,8 +1,39 @@
+using System;
+using System.Collections.Generic;
 
 namespace Caro.Shared.Game
 {
     public class BoardController
     {
-        public bool MakeMove(int x, int y) => true;
+        private int[,] board;
+        public int Size { get; private set; }
+
+        private Stack<(int x, int y)> history = new Stack<(int, int)>();
+
+        public BoardController(int size = 20)
+        {
+            Size = size;
+            board = new int[size, size];
+        }
+
+        // 0 = trống, 1 = player1, 2 = player2
+        public int GetCell(int x, int y)
+        {
+            return board[x, y];
+        }
+
+        public bool IsValidMove(int x, int y)
+        {
+            return x >= 0 && y >= 0 && x < Size && y < Size && board[x, y] == 0;
+        }
+
+        public bool MakeMove(int x, int y, int player)
+        {
+            if (!IsValidMove(x, y)) return false;
+
+            board[x, y] = player;
+            history.Push((x, y));
+            return true;
+        }
     }
 }
