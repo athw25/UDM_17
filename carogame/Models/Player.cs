8e9f5a63b64d5eb38429be0a6cbf2fe81b5bb25f
hoanganhthu-a <htat.athw11@gmail.com>
2026-04-07 20:05:10 +0700

    Add files via upload

---
+++ carogame/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carogame.Models
+{
+    public class Player
+    {
+        public string Username { get; set; }
+        public string ConnectionId { get; set; }
+        public bool IsPlaying { get; set; }
+
+        public Action<object> Send { get; set; }
+    }
+}
