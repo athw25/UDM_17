b6290ce29b526ce4aa88b80e00a9702195618b09
triet <triettm1833@ut.edu.vn>
2026-04-07 16:32:47 +0700

    Refactor BoardController to use moveHistory stack

--- Code/NetworkCaroGame/src/Caro.Shared/Game/BoardController.cs
+++ Code/NetworkCaroGame/src/Caro.Shared/Game/BoardController.cs
@@ -1,25 +1,18 @@
 using System;
 using System.Collections.Generic;
 
-namespace Caro.Shared.Game
+namespace Caro.Server.Game
 {
     public class BoardController
     {
         private int[,] board;
         public int Size { get; private set; }
+        private Stack<(int x, int y)> moveHistory = new Stack<(int, int)>();
 
-        private Stack<(int x, int y)> history = new Stack<(int, int)>();
-
-        public BoardController(int size = 20)
+        public BoardController(int size)
         {
             Size = size;
             board = new int[size, size];
-        }
-
-        // 0 = trống, 1 = player1, 2 = player2
-        public int GetCell(int x, int y)
-        {
-            return board[x, y];
         }
 
         public bool IsValidMove(int x, int y)
@@ -30,10 +23,17 @@ namespace Caro.Shared.Game
         public bool MakeMove(int x, int y, int player)
         {
             if (!IsValidMove(x, y)) return false;
-
             board[x, y] = player;
-            history.Push((x, y));
+            moveHistory.Push((x, y));
             return true;
+        }
+
+        public (int x, int y)? Undo()
+        {
+            if (moveHistory.Count == 0) return null;
+            var move = moveHistory.Pop();
+            board[move.x, move.y] = 0;
+            return move;
         }
     }
 }
