e32ababcff0b99c624057118b2e5a4d33bf9f570
hoanganhthu-a <htat.athw11@gmail.com>
2026-04-11 21:13:04 +0700

    update code

--- Code/NetworkCaroGame/src/Caro.Server/Services/MatchmakingService.cs
+++ Code/NetworkCaroGame/src/Caro.Server/Services/MatchmakingService.cs
@@ -1,12 +1,20 @@
 using System;
+using System.Collections.Generic;
 using Caro.Server.Core;
 using Caro.Shared.Network;
+using Caro.Shared.Utils;
 
 namespace Caro.Server.Services
 {
     public class MatchmakingService
     {
         private ServerManager _server;
+
+        // Danh sách người chơi đang chờ ghép
+        private List<ClientHandler> _waitingPlayers = new List<ClientHandler>();
+
+        // Danh sách phòng đang chơi
+        private List<GameRoom> _rooms = new List<GameRoom>();
 
         public MatchmakingService(ServerManager server)
         {
@@ -15,17 +23,102 @@ namespace Caro.Server.Services
 
         public void HandlePacket(ClientHandler client, Packet packet)
         {
-            // TODO (Thành viên 2): Xử lý logic thách đấu ghép phòng ở đây
+            switch (packet.Command)
+            {
+                case CommandType.ChallengeRequest:
+                    HandleFindMatch(client);
+                    break;
+
+                case CommandType.Move:
+                case CommandType.Chat:
+                case CommandType.GameOver:
+                case CommandType.TimerTick:
+                case CommandType.TimeOut:
+                    ForwardToRoom(client, packet);
+                    break;
+            }
+        }
+
+        // MATCHMAKING
+        private void HandleFindMatch(ClientHandler client)
+        {
+            Console.WriteLine($"{client.PlayerInfo.Name} is finding match...");
+
+            if (_waitingPlayers.Contains(client))
+                return;
+
+            if (_waitingPlayers.Count > 0)
+            {
+                // Lấy người chơi đầu tiên trong hàng đợi
+                var opponent = _waitingPlayers[0];
+                _waitingPlayers.RemoveAt(0);
+
+                // Tạo phòng
+                var room = new GameRoom(opponent, client, this);
+                _rooms.Add(room);
+
+                Console.WriteLine($"Match found: {opponent.PlayerInfo.Name} vs {client.PlayerInfo.Name}");
+
+                room.StartGame();
+            }
+            else
+            {
+                _waitingPlayers.Add(client);
+                Console.WriteLine($"{client.PlayerInfo.Name} is waiting...");
+            }
+        }
+
+        // FORWARD PACKET
+        private void ForwardToRoom(ClientHandler client, Packet packet)
+        {
+            var room = FindRoomByClient(client);
+            if (room != null)
+            {
+                room.HandlePacket(client, packet);
+            }
         }
 
         public void HandleDisconnect(ClientHandler client)
         {
-            // TODO (Thành viên 2): Xử lý khi người chơi thoát
+            Console.WriteLine($"{client.PlayerInfo.Name} disconnected");
+
+            // Nếu đang chờ thì xóa khỏi hàng đợi
+            _waitingPlayers.Remove(client);
+
+            var room = FindRoomByClient(client);
+            if (room != null)
+            {
+                var opponent = room.Player1 == client ? room.Player2 : room.Player1;
+
+                // báo cho đối thủ
+                opponent.SendPacket(new Packet
+                {
+                    Command = CommandType.PlayerDisconnected,
+                    Payload = ""
+                });
+
+                _rooms.Remove(room);
+            }
         }
 
         public void EndGame(GameRoom room)
         {
-            // TODO (Thành viên 2): Xử lý giải phóng phòng khi kết thúc game
+            Console.WriteLine("Ending game...");
+
+            if (_rooms.Contains(room))
+            {
+                _rooms.Remove(room);
+            }
+        }
+
+        private GameRoom FindRoomByClient(ClientHandler client)
+        {
+            foreach (var room in _rooms)
+            {
+                if (room.Player1 == client || room.Player2 == client)
+                    return room;
+            }
+            return null;
         }
     }
 }
